2daa34c328ad2a9ee0f33028e1beb6b8fa9327f1
Googol2002 <xingyuncao2002@outlook.com>
2022-11-19 15:36:08 +0800

    Update pedestrian_data.py

--- pedestrian_data.py
+++ pedestrian_data.py
@@ -1,3 +1,4 @@
+import os
 import json
 import codecs
 
@@ -5,3 +6,10 @@ PEDESTRIAN_DATA_PATH = None
 with codecs.open(r"config/config.json", 'r', 'utf-8') as config_file:
     config_data = json.load(config_file)
     PEDESTRIAN_DATA_PATH = config_data["Data-Path"]
+
+
+__SCENARIOS_FILTERED = {".git"}
+scenarios = {scenario: [sample for sample in os.listdir(os.path.join(PEDESTRIAN_DATA_PATH, scenario))
+                        if os.path.isdir(os.path.join(PEDESTRIAN_DATA_PATH, scenario, sample))] for scenario in
+             filter(lambda f: f not in __SCENARIOS_FILTERED, os.listdir(PEDESTRIAN_DATA_PATH))
+             if os.path.isdir(os.path.join(PEDESTRIAN_DATA_PATH, scenario))}
