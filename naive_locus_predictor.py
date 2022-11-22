edd97e34e9e3fccefbf3d8dbced786fa73f6f93d
Googol2002 <xingyuncao2002@outlook.com>
2022-11-22 20:27:11 +0800

    更新了一个朴素的预测器，更新了Jupyter脚本

--- naive_locus_predictor.py
+++ naive_locus_predictor.py
@@ -22,12 +22,14 @@ def __rotation_z(theta):
 
 
 """
-一个朴素的预测模型（对于x的预测还不是很准，但是对于姿态预测不错）
+一个朴素的预测模型（对于p的预测还不是很准，但是对于姿态预测不错）
+即使不涉及姿态，p仍然不准，比如在桌面上画正方形，加入卡尔曼滤波试试看
+
 @:parameter theta: 从世界坐标系，旋转到当前坐标系的极角（手机头与地磁的夹角）
 @:parameter phi: 从世界坐标系，旋转到当前坐标系的仰角（手机头与地平面的夹角）
 """
-def predict(locus: PedestrianLocus, theta, phi):
-    x, v = np.zeros((3, 1)), np.zeros((3, 1))   # 获取一个初态
+def predict(locus: PedestrianLocus, theta, phi, no_rotation=False):
+    p, v = np.zeros((3, 1)), np.zeros((3, 1))   # 获取一个初态
 
     # 这里的姿态矩阵定义是：R^{EARTH}_{IMU}，因此p^{EARTH} = R^{EARTH}_{IMU} p^{IMU}
     # imu_to_earth = __rotation_y(-phi) @ __rotation_z(-theta)\
@@ -38,28 +40,39 @@ def predict(locus: PedestrianLocus, theta, phi):
     time_frame = locus.data["Gyroscope"][:, 0]
 
     thetas, phis = np.empty(len(time_frame) - 2), np.empty(len(time_frame) - 2)
+    positions, speeds, accelerations = np.empty((len(time_frame) - 2, 3)),\
+                                       np.empty((len(time_frame) - 2, 3)),\
+                                       np.empty((len(time_frame) - 2, 3))
+
     for index, (gyroscope_imu, acceleration_imu) in enumerate(
             zip(gyroscope_imu_frame[1: -1], acceleration_imu_frame[1: -1])):
         delta_t = (time_frame[index + 2] - time_frame[index]) / 2
 
         # 姿态变化
-        rotation = __rotation_x(delta_t * gyroscope_imu[0]) \
-            @ __rotation_y(delta_t * gyroscope_imu[1]) \
-            @ __rotation_z(delta_t * gyroscope_imu[2])
+        if not no_rotation:
+            rotation = __rotation_x(delta_t * gyroscope_imu[0]) \
+                @ __rotation_y(delta_t * gyroscope_imu[1]) \
+                @ __rotation_z(delta_t * gyroscope_imu[2])
+        else:
+            rotation = np.eye(3)
 
         # 牛顿力学
         acceleration_earth = imu_to_earth @ np.expand_dims(acceleration_imu, 1)
-        x += v * delta_t + acceleration_earth * (delta_t ** 2) / 2
+        p += v * delta_t + acceleration_earth * (delta_t ** 2) / 2
         v += acceleration_earth * delta_t
 
         imu_to_earth = imu_to_earth @ rotation
 
+        # 做记录
         thetas[index] = atan2(-imu_to_earth[0, 1], imu_to_earth[1, 1])
         phis[index] = atan2(-imu_to_earth[2, 0], imu_to_earth[2, 2])
+        positions[index] = p.T
+        speeds[index] = v.T
+        accelerations[index] = acceleration_earth.T
 
-    return x, v, imu_to_earth, thetas, phis
+    return positions, speeds, accelerations, thetas, phis
 
 
 if __name__ == "__main__":
-    dataset = PedestrianDataset(["Magnetometer"])
+    dataset = PedestrianDataset(["Magnetometer"], gps_preprocessed=False)
     predict(dataset["NorthEastSouthWest"], 0, 0)
