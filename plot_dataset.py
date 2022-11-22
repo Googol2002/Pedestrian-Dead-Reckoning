4ba64a1234ba223d3893aa256a8d059f287b4a7a
fireball0213 <1306189897@qq.com>
2022-11-22 10:54:59 +0800

    简单的plot封装

--- plot_dataset.py
+++ plot_dataset.py
@@ -12,23 +12,26 @@ import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 
 # 步行轨迹图
-# l, r = (0, 5)  # 画图范围
-# CUT_BEGIN = 10  # 删掉前几秒的数据，因为GPS不准
-#
+CUT_BEGIN = 10  # 删掉前几秒的数据，因为GPS不准
+def plot_locus(Lati,Longi,label="TBD",cut_begin=CUT_BEGIN):
+    Lati = Lati[cut_begin:]
+    Longi = Longi[cut_begin:]
+    plt.xlabel("Latitude (°)")
+    plt.ylabel("Longitude (°)")
+    plt.text(Lati[0], Longi[0], 's', fontsize=10)
+    plt.text(Lati[-1], Longi[-1], 'e', fontsize=10)
+    plt.plot(Lati, Longi, '+', markersize=1, label=label)
+    plt.legend(loc="upper right")
+#l, r = (0, 5)  # 画图范围
 # dataset = PedestrianDataset(["test_case0"], window_size=100)  # 指定文件夹
 # for num, (name, locus) in enumerate(dataset):
 #     if num in range(l, r):
 #         print("正在遍历移动轨迹{}... \n".format(name))
 #         locus_pair = np.array(list(zip(locus.y_frame["Latitude (°)"], locus.y_frame["Longitude (°)"])))
 #         print("轨迹长度: ", len(locus_pair))
-#         Lati = locus_pair.T[0][CUT_BEGIN:]
-#         Longi = locus_pair.T[1][CUT_BEGIN:]
-#         plt.xlabel("Latitude (°)")
-#         plt.ylabel("Longitude (°)")
-#         plt.text(Lati[0], Longi[0], 's', fontsize=10)
-#         plt.text(Lati[-1], Longi[-1], 'e', fontsize=10)
-#         plt.plot(Lati, Longi, '+', markersize=1, label="{}".format(name))
-#         plt.legend(loc="upper right")
+#         Lati = locus_pair.T[0]
+#         Longi = locus_pair.T[1]
+#         plot_locus(Lati,Longi, label="{}".format(name))
 #     if num >= r:
 #         break
 # plt.show()
@@ -37,24 +40,25 @@ matplotlib.use('TkAgg')
 
 
 # 重力加速度变化图
+def plot_gravity(sample:dict,title="TBD"):
+    a = sample['Accelerometer']
+    b = sample['Linear Acceleration']
+    minus = (a - b)
+    c = np.array(list(map(lambda x: np.linalg.norm(x), minus)))
+    print(c.mean())
+    plt.plot(range(len(c)), c)
+    plt.title(title)
+
 l, r = (10, 14)  # 画图范围
 dataset = PedestrianDataset(["Hand-Walk"], window_size=10000)
 
 for num, (name, locus) in enumerate(dataset):
     if num in range(l, r):
         print("正在遍历移动轨迹{}... \n".format(name))
-        plt.subplot(22 * 10 + num - 10 + 1)
+        plt.subplot(22 * 10 + num%10 + 1)
         for sample in locus:
             print(len(locus))
-            # print(sample.keys())
-            a = sample['Accelerometer']
-            b = sample['Linear Acceleration']
-            minus = (a - b)
-            c = np.array(list(map(lambda x: np.linalg.norm(x), minus)))
-            print(c.mean())
-            plt.plot(range(len(c)), c)
-            plt.title(label="{}".format(name))
-            # plt.legend(loc="upper right")
+            plot_gravity(sample,title="{}".format(name))
             break
     if num >= r:
         break
