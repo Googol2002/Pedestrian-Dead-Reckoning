b309f0ffb425cf6791f9b93c71392e4ad87ff9c3
Lqf-HFNJU <201830168@smail.nju.edu.cn>
2022-11-26 19:23:21 +0800

    基于加速度的步幅预测

--- pedestrian_data.py
+++ pedestrian_data.py
@@ -13,7 +13,7 @@ from geopy.distance import geodesic
 
 with codecs.open(r"config/config.json", 'r', 'utf-8') as config_file:
     config_data = json.load(config_file)
-    PEDESTRIAN_DATA_PATH = config_data["lqf-Data-Path"]
+    PEDESTRIAN_DATA_PATH = config_data["Data-Path"]
 
 __SCENARIOS_FILTERED = {".git"}
 _scenarios = {scenario: [sample for sample in os.listdir(os.path.join(PEDESTRIAN_DATA_PATH, scenario))
