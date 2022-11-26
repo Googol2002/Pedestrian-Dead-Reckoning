3a50e3ae7617767f7d422a108b9e983c4bcaa0d7
Googol2002 <xingyuncao2002@outlook.com>
2022-11-26 19:59:04 +0800

    增加了GPS的插值算法

--- locus_predictor/mature_locus_predictor.py
+++ locus_predictor/mature_locus_predictor.py
@@ -1,4 +1,5 @@
 from numpy import cos, sin
+from scipy.interpolate import interp1d
 from scipy.spatial.transform import Rotation
 
 import numpy as np
@@ -28,6 +29,8 @@ PACE_STEP = 0.8
 @:return positions: 利用步频步幅得到数据插值而来
 @:return properties: 一些属性
 """
+
+
 def predict(locus: PedestrianLocus, attitude=None, moving_bias=0, pace_inference=None):
     p, v = np.zeros(3), np.zeros(3)  # 获取一个初态
     theta, phi = attitude if attitude else measure_initial_attitude(locus, 30)
@@ -36,11 +39,13 @@ def predict(locus: PedestrianLocus, attitude=None, moving_bias=0, pace_inference
     imu_to_earth = Rotation.from_euler("ZYX", [theta, 0, phi])
 
     gyroscope_imu_frame, magnetometer_imu_frame, acceleration_imu_frame = locus.data["Gyroscope"][:, 1:], \
-        locus.data["Magnetometer"][:, 1:], locus.data["Linear Acceleration"][:, 1:]
+                                                                          locus.data["Magnetometer"][:, 1:], locus.data[
+                                                                                                                 "Linear Acceleration"][
+                                                                                                             :, 1:]
     time_frame = locus.data["Gyroscope"][:, 0]
 
     thetas, phis, alphas, directions = [np.empty(len(time_frame) - 2) for _ in range(4)]
-    positions, speeds, accelerations = [np.empty((len(time_frame) - 2, 3)) for _ in range(3)]
+    speeds, accelerations = [np.empty((len(time_frame) - 2, 3)) for _ in range(2)]
     for index, (gyroscope_imu, acceleration_imu, magnetometer_imu) in enumerate(
             zip(gyroscope_imu_frame[1: -1], acceleration_imu_frame[1: -1], magnetometer_imu_frame[1: -1])):
         delta_t = (time_frame[index + 2] - time_frame[index]) / 2
@@ -58,7 +63,6 @@ def predict(locus: PedestrianLocus, attitude=None, moving_bias=0, pace_inference
         v += acceleration_earth * delta_t
 
         # 记录运动学信息
-        # positions[index] = p
         speeds[index] = v
         accelerations[index] = acceleration_earth
         directions[index] = thetas[index]
@@ -69,30 +73,34 @@ def predict(locus: PedestrianLocus, attitude=None, moving_bias=0, pace_inference
     info = {"speeds": speeds, "accelerations": accelerations,
             "thetas": thetas, "phis": phis, "time": time_frame[1:-1],
             "peaks": peaks_index, "directions": directions,
-            "walk_time": time_frame[1 + peaks_index],
-            "locus":locus}
+            "walk_time": time_frame[1 + peaks_index], "locus": locus}
     # 步幅步频
     inference = pace_inference(info) if pace_inference else lambda x, y: PACE_STEP
     walk_positions = np.zeros((len(peaks_index) + 1, 2))
     walk_directions = np.zeros(len(peaks_index))
     p = np.zeros(2)
-
     for index, peak in enumerate(peaks_index):
         direction = directions[peak]
         walk_directions[index] = direction
 
         pace = inference(index, peak)
-        p += pace * np.asarray([cos(np.pi/2 + direction), sin(np.pi/2 + direction)])
+        p += pace * np.asarray([cos(np.pi / 2 + direction), sin(np.pi / 2 + direction)])
         walk_positions[index + 1] = p
 
     info["walk_positions"] = walk_positions
     info["walk_directions"] = walk_directions
 
     # 插值
+    if len(peaks_index) > 3:
+        positions = interp1d(np.concatenate((np.array([0]), time_frame[1 + peaks_index])),
+                             walk_positions, kind='cubic', axis=0, fill_value="extrapolate")\
+            (locus.y_frame["location_time"])
+    else:
+        positions = None
     # 汇总数据
-    return positions[:, :2], info
+    return positions, info
 
 
 if __name__ == "__main__":
     dataset = PedestrianDataset(["Magnetometer"])
-    predict(dataset["随机漫步1"])
+    predict(dataset["NorthEastSouthWest"])
