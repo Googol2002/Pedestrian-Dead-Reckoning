6cae1f3407da147298434e61a96760abead57f6d
fireball0213 <1306189897@qq.com>
2022-11-27 19:37:54 +0800

    简单修改

--- pace_predictor/acc_pace_inference.py
+++ pace_predictor/acc_pace_inference.py
@@ -5,8 +5,9 @@ import numpy as np
 
 MIN_PERIOD = 20
 PROMINENCE = (0.05, None)
-
-
+Magic_A=0.37
+Magic_B=0.000155
+Magic_C=0.1638
 def pace_inference(info):
     accelerations = info["accelerations"]
     a_vec = np.sqrt(np.power(accelerations[:, 0], 2) + np.power(accelerations[:, 1], 2))
@@ -14,14 +15,16 @@ def pace_inference(info):
     peaks_index, _ = find_peaks(a_vec, distance=MIN_PERIOD, prominence=PROMINENCE)
     walk_time = time[peaks_index]
     steplength = []
+    magic_a,magic_b,magic_c=Magic_A,Magic_B,Magic_C
     for i in range(len(walk_time) - 1):
         W = (walk_time[i] if i == 0 else walk_time[i + 1] - walk_time[i]) * 1000
         peak = (a_vec[peaks_index[i + 1]] + a_vec[peaks_index[i]]) / 2
         valley = np.min(a_vec[peaks_index[i]:peaks_index[i + 1]])
         H = peak - valley
-
-        steplength.append(0.37 - 0.000155 * W + 0.1638 * math.sqrt(H))
-    print(np.asarray(steplength).mean())
+        magic_num=magic_a - magic_b * W + magic_c * math.sqrt(H)
+        steplength.append(magic_num)
+    steplength = np.array(steplength).clip(0.6, 0.75)
+    print("steplength.mean()",steplength.mean())
 
     def inference(index, peak):
         a_earth = accelerations[peak]
